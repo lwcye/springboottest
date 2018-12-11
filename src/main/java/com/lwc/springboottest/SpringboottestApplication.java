d6e7bef329dbb88bc28b75a14f139e4e07d8040c
caoliewei <15213024302@139.com>
2018-12-11 14:02:28 +0800

    Initial commit

---
+++ src/main/java/com/lwc/springboottest/SpringboottestApplication.java
@@ -0,0 +1,20 @@
+package com.lwc.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringboottestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboottestApplication.class, args);
+    }
+
+    @RequestMapping("/hi")
+    public String hello() {
+        return "hello,you are welcome";
+    }
+}
